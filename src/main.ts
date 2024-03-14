f84cab8eed7c7838eb1e66896f4fc424801dd06d
bwardak <baheer.wardak06@gmail.com>
2024-03-14 20:56:53 +0000

    Added the changed woods screen. Started to make objects to put items in rather than manually change within each function.

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,41 @@ if (!logText) {
   throw new Error("Error with Log Text selector");
 }
 
-gameContainer.style.display = "none"   // Uncomment this in the end and remove the line below
-// buttonStartGame.style.display = "none"
+const locationImage = document.querySelector<HTMLImageElement>(".location-image")
+if (!locationImage) {
+  throw new Error("Error with Image Container selector");
+}
+
+const narrativeText = document.querySelector<HTMLParagraphElement>(".narrative-text")
+if (!narrativeText) {
+  throw new Error("Error with Narrative Text selector");
+}
+
+const backgroundColor = document.querySelector<HTMLDivElement>("#background")
+if (!backgroundColor) {
+  throw new Error("Error with Background Color selector");
+}
+
+// gameContainer.style.display = "none"   // Uncomment this in the end and remove the line below
+buttonStartGame.style.display = "none"
+
+type travel = {
+  name: string;
+  "button text": string;
+  "button functions"?: void;
+  areaText: string;
+}
+
+const travel = [
+  {
+    name: "Woods",
+    "button text": "Woods",
+    // "button function": ,
+    text: "As you step into the woods, a chill runs down your spine, the canopy above casting the forest floor in a dim, dappled light. The ancient trees loom over you like silent sentinels, their gnarled branches reaching out as if to grasp at your very essence. The air is thick with the scent of damp earth and decaying leaves, and every rustle of movement sets your heart racing. You tread carefully, the path winding ahead, each twist and turn a potential new discovery or danger lurking in the shadows."
+  }
+]
+  
+
 
 const handleStartOfGameScreen = () => {
   gameContainer.style.display = "initial"
@@ -44,7 +77,25 @@ const handleTravelMenu = () => {
   buttonCrafting.style.display = "none";
   buttonInfo.innerText = "Where to: "
   logText.innerText = "You have chosen to set out..."
+
+  buttonTravel.removeEventListener("click", handleTravelMenu)
+  buttonTravel.addEventListener("click", handleGoToWoods)
 };
+
+const handleGoToWoods = () => {
+  locationImage.src = "https://t3.ftcdn.net/jpg/05/62/56/46/360_F_562564643_OSsBfTgR7mLjKtY5TCHrwGA2auYkou2T.jpg";
+  narrativeText.innerText = "As you step into the woods, a chill runs down your spine, the canopy above casting the forest floor in a dim, dappled light. The ancient trees loom over you like silent sentinels, their gnarled branches reaching out as if to grasp at your very essence. The air is thick with the scent of damp earth and decaying leaves, and every rustle of movement sets your heart racing. You tread carefully, the path winding ahead, each twist and turn a potential new discovery or danger lurking in the shadows."
+  backgroundColor.style.backgroundColor = "#111a10";
+  buttonInfo.innerText = "Choose an action...";
+  buttonCrafting.style.display = "initial";
+  buttonCrafting.innerText = "Gather stone";
+  buttonTravel.innerText = "Gather wood"
+  logText.innerText = "You enter the still woods..."
+
+
+  buttonTravel.removeEventListener("click", handleTravelMenu);
+  buttonTravel.addEventListener("click", handleGoToWoods);
+}
 
 buttonStartGame.addEventListener("click", handleStartOfGameScreen)
 
